686fddc2151ba9c7d014f94bbf0819a34d02548e
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-07 09:02:56 -0300

    Exercises coding girls

---
+++ Exercícios Coding Girls/Aula 1/Aula1Ex2.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Program
+{
+  public class Aula1
+  {
+    class Ex2
+    {
+      static void Main (string[] args)
+      {
+        string nome = "Carol Shaw";
+        string formacao = "Engenheira da computacao";
+        string contribuicao = "Primeira mulher que comecou a trabalhar com o desenvolvimento de jogos digitais";
+        
+	    Console.WriteLine ($"{nome}\n{formacao}\n{contribuicao}");
+      }
+    }
+  }
+}
+
---
+++ Exercícios Coding Girls/Aula 1/Aula1Ex4.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Program
+{
+  public class Aula1
+  {
+    class Ex4
+    {
+      static void Main (string[] args)
+      {
+        string mensagem = "Lugar de mulher é aonde ela quiser. Vai ser difícil no começo, mas no final tudo dará certo!";
+        
+	    Console.WriteLine ($"{mensagem}");
+      }
+    }
+  }
+}

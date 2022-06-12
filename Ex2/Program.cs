eb48649c03dc99d1950da4be389f65114de07fa8
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-12 18:55:03 -0300

    exercises

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira tres valores diferentes: ");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+            int valor3 = Convert.ToInt32(Console.ReadLine());
+
+            if (valor3 > valor2 && valor3 > valor1)
+            {
+                Console.WriteLine($"{valor3} é o maior.");
+            }
+            else if (valor2 > valor1 && valor2 > valor3)
+            {
+                Console.WriteLine($"{valor2} é o maior.");
+            }
+            else
+            {
+                Console.Write($"{valor1} é o maior.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira tres notas de um aluno: ");
+            double nota1 = double.Parse(Console.ReadLine());
+            double nota2 = double.Parse(Console.ReadLine());
+            double nota3 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine($"O aluno tirou {media} e foi aprovado.");
+            }
+            else
+                Console.WriteLine($"O aluno tirou {media} e foi reprovado.");
+            Console.ReadLine();
+        }
+    }
+}

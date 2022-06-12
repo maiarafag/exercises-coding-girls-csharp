2b956ec4da415c8efc7003daf82ff19dc43fc779
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-12 19:00:07 -0300

    exercises

---
+++ Aula3/Ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira tres notas de um aluno: ");
+            double nota1 = double.Parse(Console.ReadLine());
+            double nota2 = double.Parse(Console.ReadLine());
+            double nota3 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 9.0 && media <= 10.0)
+            {
+                Console.WriteLine($"O aluno tirou A. Parabéns!");
+            }
+            else if (media >= 8.0 && media < 9.0)
+            {
+                Console.WriteLine($"O aluno tirou B. Parabéns!");
+            }
+            else if (media >= 7.0 && media < 8.0)
+            {
+                Console.WriteLine($"O aluno tirou C. Parabéns!");
+            }
+            else if (media >= 6.0 && media < 7.0)
+            {
+                Console.WriteLine($"O aluno tirou D. Dessa vez foi por pouco!");
+            }
+            else
+                Console.WriteLine($"O aluno tirou F. Tente outra vez!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula3/Ex5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do salario: ");
+            double salario = double.Parse(Console.ReadLine());
+
+
+            if (salario > 0 && salario <= 400.00)
+            {
+                double p = 0.15;
+                double reajuste = (p * salario);
+                double salarionovo = reajuste + salario;
+
+                Console.WriteLine(string.Format("Reajuste ganho: {0:0.00}", reajuste));
+                Console.WriteLine(string.Format("Novo salario: {0:0.00}", salarionovo));
+                Console.WriteLine($"Em percentual: 15 %");
+            }
+            else if (salario > 400.01 && salario <= 800.00)
+            {
+                double p = 0.12;
+                double reajuste = (p * salario);
+                double salarionovo = reajuste + salario;
+
+                Console.WriteLine(string.Format("Novo salario: {0:0.00}", salarionovo));
+                Console.WriteLine(string.Format("Reajuste ganho: {0:0.00}", reajuste));
+                Console.WriteLine($"Em percentual: 12 %");
+            }
+            else if (salario > 800.01 && salario <= 1200.00)
+            {
+                double p = 0.10;
+                double reajuste = (p * salario);
+                double salarionovo = reajuste + salario;
+
+                Console.WriteLine(string.Format("Novo salario: {0:0.00}", salarionovo));
+                Console.WriteLine(string.Format("Reajuste ganho: {0:0.00}", reajuste));
+                Console.WriteLine($"Em percentual: 10 %");
+            }
+            else if (salario > 1200.01 && salario <= 2000.00)
+            {
+                double p = 0.07;
+                double reajuste = (p * salario);
+                double salarionovo = reajuste + salario;
+
+                Console.WriteLine(string.Format("Novo salario: {0:0.00}", salarionovo));
+                Console.WriteLine(string.Format("Reajuste ganho: {0:0.00}", reajuste));
+                Console.WriteLine($"Em percentual: 7 %");
+
+            }
+            else
+            {
+                double p = 0.04;
+                double reajuste = (p * salario);
+                double salarionovo = reajuste + salario;
+
+                Console.WriteLine(string.Format("Novo salario: {0:0.00}", salarionovo));
+                Console.WriteLine(string.Format("Reajuste ganho: {0:0.00}", reajuste));
+                Console.WriteLine($"Em percentual: 4 %");
+            }
+            Console.ReadLine();
+        }
+    }
+}

dd866087c7d7e3253594da8310b889afa979e929
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-12 19:01:18 -0300

    exercises

---
+++ Aula4/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valoresa = new int[10];
+            int[] valoresb = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite um numero: ");
+                valoresa[i] = int.Parse(Console.ReadLine());
+                valoresb[i] = (valoresa[i] * 5);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"O valor {valoresa[i]} multiplicado por 5 e igual a {valoresb[i]}.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Aula4/Ex2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valorpar = new int[10];
+            int[] valorimpar = new int[10];
+            int num;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite um numero: ");
+                num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    valorpar[i] = num;
+                }
+                else
+                {
+                    valorimpar[i] = num;
+                }
+
+            }
+            Console.WriteLine("Os numeros pares sao: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (valorpar[i] != 0)
+                {
+                    Console.WriteLine(valorpar[i] + " é par");
+                }
+            }
+            Console.WriteLine("Os numeros impares sao: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (valorimpar[i] != 0)
+                {
+                    Console.WriteLine(valorimpar[i] + " é impar");
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}

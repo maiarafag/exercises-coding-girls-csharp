c105d5b8745a77d962a41cd321916208fcc25cc4
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-12 18:50:41 -0300

    exercises

---
+++ Aula2/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu ano de nascimento:");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+
+            int idade = 2022 - ano;
+
+            Console.WriteLine($"Você tem ou terá {idade} anos neste ano.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula2/Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira dez valores: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+            int f = Convert.ToInt32(Console.ReadLine());
+            int g = Convert.ToInt32(Console.ReadLine());
+            int h = Convert.ToInt32(Console.ReadLine());
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            int soma = a + b + c + d + e + f + g + h + i + j;
+
+            Console.WriteLine($"A soma dos números será: {soma}");
+            Console.ReadLine();
+        }
+    }
+}

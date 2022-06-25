effcc34e8af51bccf67268cc441529a258b9cc11
Maiara Fagundes <maiara.fag@hotmail.com>
2022-06-25 14:54:19 -0300

    exercises

---
+++ Aula8/Ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Maiara";
+            cliente.Cpf = "12345678911";
+            Console.WriteLine("---Dados do Cliente---");
+            Console.WriteLine("Nome: " + cliente.Nome + "\nCPF: " + cliente.Cpf);
+
+            Produto produto = new Produto();
+            produto.Item = "Detergente";
+            produto.CodigoProduto = "123";
+            produto.EspecificacaoProduto = "Produto de Limpeza";
+            produto.Quantidade = 2;
+            produto.PrecoProduto = 2.00;
+            produto.ValorTotal = 4.00;
+            produto.FormaPagamento = "Dinheiro";
+            Console.WriteLine("\n---Dados da Compra---");
+            Console.WriteLine("Código do Produto: " + produto.CodigoProduto + "\nEspecificação do Produto: " + produto.EspecificacaoProduto
+             + "\nProduto: " + produto.Item + "\nQuantidade: " + produto.Quantidade + "\nPreço Unitário: R$ " + produto.PrecoProduto +
+             "\nValor Total: R$ " + produto.ValorTotal + "\nForma de Pagamento: " + produto.FormaPagamento);
+
+
+        }
+    }
+}
---
+++ Aula8/Ex4/Supermercado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    public class Supermercado
+    {
+        public double PrecoProduto { set; get; }
+        public string CodigoProduto { set; get; }
+        public int Quantidade { set; get; }
+        public string Item { set; get; }
+        public string EspecificacaoProduto { set; get; }
+        public double ValorTotal { set; get; }
+        public string FormaPagamento { set; get; }
+
+
+    }
+
+   public class Cliente
+    {
+        public string Nome  { set; get; }
+        public string Cpf  { set; get; }
+
+    }
+
+   public  class Produto : Supermercado
+    {
+        
+         public string Descricao()
+        {
+            return "Produto";
+        }
+    }
+
+   public  class Pedido : Supermercado
+    {
+        public string Descricao()
+        {
+            return "Pedido";
+        }
+        
+    }
+}
